36a60393ab3ec9727cba6ae8483d7e0a30504bd8
sid2710-alt <73985972+sid2710-alt@users.noreply.github.com>
2022-03-20 12:29:08 +0530

    updatedmongo

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose= require('mongoose');
-mongoose.connect("mongodb://localhost/Ecom");
+mongoose.connect("mongodb+srv://siddhu:siddhu@cluster0.9yjwk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
 const db=mongoose.connection;
 db.on('error',console.error.bind(console,"error connecting to database"));
 db.once('open',function(){
